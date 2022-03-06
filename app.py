23ad678f2bc7cad5637bf214652668505c508b8e
Andres Gomez <andres@mirocard.swiss>
2022-03-06 16:30:14 +0100

    updates layout. title is now updated with values

--- app.py
+++ app.py
@@ -6,52 +6,58 @@ import numpy as np
 
 
 ########### Define your variables
-mytitle = 'Availability Graph'
-tabtitle = 'Graph'
-myheading = 'Dependency between failure and recovery rates'
-label1 = 'hours'
-githublink = 'https://github.com/dflymegold/availability-graph'
+X_MAX = 10
+X_STEP = 0.1
+tabtitle = "Availability Graph"
 
 ########### Set up the chart
-
 
 fig = go.Figure()
 
 def lambda_arg (step_):
-    return ((1-10)/step_)
+    return (step_)
 def mu_arg (step_):
-    return (1/step_)
+    return (1)
 
-for step in np.arange(0,101,0.5):
+for step in np.arange(0,X_MAX, X_STEP):
+    coeff1 = (mu_arg(step)/(lambda_arg(step)+mu_arg(step)))
+    coeff2 = (lambda_arg(step)/(lambda_arg(step)+mu_arg(step)))
+    coeff3 = (lambda_arg(step)+mu_arg(step))
     fig.add_trace(
         go.Scatter(
             visible=False,
             line=dict(color="#027D3F", width=6),
-            name="𝜈 = " + str(step),
-            x=np.arange(0,101,1),
-            y = ((mu_arg(step)/(lambda_arg(step)+mu_arg(step)))+(lambda_arg(step)/(lambda_arg(step)+mu_arg(step)))*
-                 1/np.exp(-((lambda_arg(step)+mu_arg(step))*np.arange(0,101,1))))))
+            name="λ = " + str(step),
+            x=np.arange(0,X_MAX, X_STEP),
+            y = ( coeff1 + coeff2 * np.exp( -coeff3 * np.arange(0,X_MAX, X_STEP) ) )
+            )
+        )
 
+#visualize first plot
 fig.data[0].visible = True
 
 steps = []
-slider_data = np.arange(0,10,1)
-#for i in range(len(fig.data)):
-for i in slider_data:
+for i in range(len(fig.data)):
+    i2 = 0.1*i
+    steady_state = mu_arg(i2) / (mu_arg(i2)+lambda_arg(i2))
+    mu_str = '<b>Repair rate:</b> {} per hour'.format(mu_arg(i2))
+    lamba_str = '<b>Failure rate:</b> {} per hour'.format(lambda_arg(i2))
+    ss_str = '<b>Steady state availability:</b> {0:.2f}'.format(steady_state)
+    title_str = " "+mu_str+"     "+lamba_str+"     "+ss_str+"<br> "
     step = dict(
         method="update",
-        args=[{"visible": [False] * len(slider_data)},
-              {"title": "λ Failure rate (failures per hour): " + str(0.1*i)}],
-        label = str("%.2f" %(0.1*i))
+        args=[{"visible": [False] * len(fig.data)},
+                { "title": title_str }],
+                label = str("%.2f" %(0.1*i))
     )
     step["args"][0]["visible"][i] = True  # Toggle i'th trace to "visible"
     steps.append(step)
 
 sliders = [dict(
     active=0,
-    currentvalue={"prefix": " λ Failure rate (failures per hour): "},
+    # currentvalue={"prefix": " λ Failure rate (failures per hour): "},
     pad={"t": 50},
-    steps = steps
+    steps = steps[::10]
 )]
 
 fig.update_layout(
@@ -59,6 +65,8 @@ fig.update_layout(
     xaxis_title="Hours",
     yaxis_title="Availability",
 )
+
+fig.update_yaxes(range=[0,1])
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -68,12 +76,9 @@ app.title = tabtitle
 
 ########### Set up the layout
 app.layout = html.Div(children=[
-#    html.H1(myheading),
     dcc.Graph(
         id='graph',
         figure=fig),
-#    html.Br(),
-#    html.A('Code on Github', href=githublink),
 ])
 
 if __name__ == '__main__':
